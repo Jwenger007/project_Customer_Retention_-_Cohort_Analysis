167300b949bd9aea15d0502485e28e8fe8fe8f85
Jwenger007 <brmdgkb2t5@privaterelay.appleid.com>
2026-02-21 22:09:48 +0100

    chart + upgraded 08 (customer livetime value)

--- 08_basic_clv.sql
+++ 08_basic_clv.sql
@@ -1,26 +1,31 @@
 -- =========================================================
 -- 08_basic_clv.sql
--- Purpose: Calculate basic customer lifetime value
+-- Purpose: Calculate CLV per cohort
 -- =========================================================
 
-DROP TABLE IF EXISTS analysis.clv_summary;
+DROP TABLE IF EXISTS analysis.clv_per_cohort;
 
-CREATE TABLE analysis.clv_summary AS
+CREATE TABLE analysis.clv_per_cohort AS
 WITH customer_revenue AS (
     SELECT
-        customerid,
-        SUM(revenue) AS total_revenue_per_customer,
-        COUNT(DISTINCT invoiceno) AS total_orders
-    FROM clean.transactions
-    GROUP BY customerid
+        t.customerid,
+        f.cohort_month,
+        SUM(t.revenue) AS total_revenue_per_customer,
+        COUNT(DISTINCT t.invoiceno) AS total_orders_per_customer
+    FROM clean.transactions t
+    JOIN analysis.customer_first_purchase f
+        ON t.customerid = f.customerid
+    GROUP BY t.customerid, f.cohort_month
 )
+
 SELECT
-    COUNT(*) AS total_customers,
-    ROUND(AVG(total_revenue_per_customer), 2) AS avg_customer_lifetime_value,
-    ROUND(AVG(total_orders), 2) AS avg_orders_per_customer,
-    ROUND(SUM(total_revenue_per_customer), 2) AS total_revenue
-FROM customer_revenue;
+    cohort_month,
+    COUNT(customerid) AS total_customers,
+    ROUND(SUM(total_revenue_per_customer), 2) AS total_revenue,
+    ROUND(AVG(total_revenue_per_customer), 2) AS avg_clv_per_customer,
+    ROUND(AVG(total_orders_per_customer), 2) AS avg_orders_per_customer
+FROM customer_revenue
+GROUP BY cohort_month
+ORDER BY cohort_month;
 
-
-SELECT * FROM analysis.clv_summary;
-
+SELECT * FROM analysis.clv_per_cohort;
